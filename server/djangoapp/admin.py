e497bcccd48a425e4431c79d1b7e395c3a744cd7
Damian Cwynar <dmn@cwnr.io>
2021-05-17 23:31:32 +0200

    Register CarModel and CarMake classes in admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from .models import CarMake, CarModel
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    model = CarModel
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    model = CarMake
+    inlines = [CarModelInline]
 
-# Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
