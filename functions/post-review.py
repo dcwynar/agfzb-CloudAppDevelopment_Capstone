b63b4bcda89083316332625cf9b158f2cebd9001
Damian Cwynar <dmn@cwnr.io>
2021-05-17 22:58:31 +0200

    Add functions for Cloudant integration... For no reason

--- functions/post-review.py
+++ functions/post-review.py
@@ -8,6 +8,26 @@
 #
 #
 import sys
+import requests
 
-def main(dict):
-    return { 'message': 'Hello world' }
+def main(params):
+    URL = params['cloudant_url'] + '/reviews'
+    if not 'review' in params.keys():
+        return {
+            'headers': {'Content-Type': 'application/json'},
+            'statusCode': 500,
+            'body': {'error' :'No review object in request'}
+        }
+    req = requests.post(URL, auth=(params['cloudant_user'], params['cloudant_password']), json=params['review'])
+    if req.status_code > 400:
+        return {
+            'headers': {'Content-Type': 'application/json'},
+            'statusCode': 500,
+            'body': {'error' :req.text}
+        }
+    payload = req.json()
+    return {
+            'headers': {'Content-Type': 'application/json'},
+            'statusCode': 200,
+            'body': payload
+    }
